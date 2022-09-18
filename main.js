7da4eebb84922032f4c96e632798dc81b2825c1a
Mo'men Ashraf <100002291+Moamen31@users.noreply.github.com>
2022-09-18 21:51:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector(".menu-btn")
+const burgerIcon = document.querySelector(".menu-btn__burger")
+
+const nav = document.querySelector(".nav")
+const menuNav = document.querySelector(".menu-nav")
+
+const navItems = document.querySelectorAll(".menu-nav__item")
+
+menuBtn.addEventListener("click", () => {
+    burgerIcon.classList.toggle("open")
+    menuNav.classList.toggle("open")
+    navItems.forEach(item => {
+        item.classList.toggle("open")
+    })
+})
